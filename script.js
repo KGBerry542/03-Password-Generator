e3a0f084389680a7ac32f74d5047bcc5cadeabe4
KGBerry542 <74325710+KGBerry542@users.noreply.github.com>
2023-02-10 18:59:19 -0500

    identifying which characters can be used in the generated password

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // Assignment code here
 
+// Characters that can be used in password generator
+let specialCharacter = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+', ':', ';', '<', '>', '/', '?'];
+let numberCharacter = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let uppercaseCharacter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let lowercaseCharacter = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
